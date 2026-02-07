2808b163f359c68a2ea022aa21dfd8cde49f3da9
IrinaIP13 <irina.ip1303@gmail.com>
2026-02-07 17:51:25 +0200

    HW-1 task-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import './App.css'
+// #gi01MZ6v
+//
+// з arrays.js взяти масив coursesTitleArray. Вивести кожен його елемент, створивши необхідні моделі та компоненти
+//
+// Стилізація довільна, але обов’язково підключена і мінімально зроблена (хоча б один стиль, який працює).
 
-function App() {
+import './App.css'
+const coursesTitleArray: string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
+
+const App = () => {
 
   return (
-    <></>
+    <>
+    <ul>
+        {
+            coursesTitleArray.map((value, index) => <li key={index}>{value}</li>)
+        }
+    </ul>
+    </>
   )
 }
 
 export default App
+
