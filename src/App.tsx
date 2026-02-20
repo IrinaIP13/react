171df898fb90824e61a669436c4b1c9dfaf5a7e7
IrinaIP13 <irina.ip1303@gmail.com>
2026-02-20 18:27:48 +0200

    HW-1 Task-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css'
+const coursesTitleArray: string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
 
-function App() {
+const App = () => {
 
-  return (
-    <></>
-  )
+    return (
+        <>
+            <ul>
+                {
+                    coursesTitleArray.map((value, index) => <li key={index}>{value}</li>)
+                }
+            </ul>
+        </>
+    )
 }
 
 export default App
